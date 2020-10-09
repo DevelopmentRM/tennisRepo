bce972405762490d02ce751a7c0ded83e4dca93d
maiNabil <mai_nabil26@hotmail.com>
2020-10-09 20:07:55 +0200

    first

---
+++ src/app/components/components.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
+import * as Rellax from 'rellax';
+
+@Component({
+    selector: 'app-components',
+    templateUrl: './components.component.html',
+    styles: [`
+    ngb-progressbar {
+        margin-top: 5rem;
+    }
+    `]
+})
+
+export class ComponentsComponent implements OnInit, OnDestroy {
+   imagelist:String[]=[];
+    data : Date = new Date();
+
+    page = 4;
+    page1 = 5;
+    page2 = 3;
+    focus;
+    focus1;
+    focus2;
+
+    date: {year: number, month: number};
+    model: NgbDateStruct;
+
+    public isCollapsed = true;
+    public isCollapsed1 = true;
+    public isCollapsed2 = true;
+
+    state_icon_primary = true;
+
+    constructor( private renderer : Renderer2, config: NgbAccordionConfig) {
+        config.closeOthers = true;
+        config.type = 'info';
+    }
+    isWeekend(date: NgbDateStruct) {
+        const d = new Date(date.year, date.month - 1, date.day);
+        return d.getDay() === 0 || d.getDay() === 6;
+    }
+
+    isDisabled(date: NgbDateStruct, current: {month: number}) {
+        return date.month !== current.month;
+    }
+
+    ngOnInit() {
+      var rellaxHeader = new Rellax('.rellax-header');
+
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.add('navbar-transparent');
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.add('index-page');
+        this.slideImages();
+    }
+    slideImages(){
+     for(let i=0;i<178;i++){
+        let imageName:String="assets\\img\\tinessimage\\IMG ("+(i+1).toString()+")"+".jpg";
+        this.imagelist.push(imageName);
+     }
+    }
+    ngOnDestroy(){
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.remove('navbar-transparent');
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.remove('index-page');
+    }
+
+}
